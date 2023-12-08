31c8256323d15475fafe84f9b7b6404a21b5b573
miklosa <79603166+miklosa@users.noreply.github.com>
2023-12-08 11:32:54 -0600

    Release/16.040 (#12)
    * 35
    
    * Release/16.030 (#9)
    
    * Change 1
    
    * Change 2
    
    * Change 3
    
    * change 4
    
    * Miklosa/release 16.030/123455 (#8)
    
    * new 1
    
    * new 2
    
    * new change 3
    
    ---------
    
    Co-authored-by: Arpad Miklos <AMiklos@myagworks.com>
    
    ---------
    
    Co-authored-by: Arpad Miklos <AMiklos@myagworks.com>
    
    * Release/16.035 (#11)
    
    * Release/16.030 (#10)
    
    * Change 1
    
    * Change 2
    
    * Change 3
    
    * change 4
    
    * Miklosa/release 16.030/123455 (#8)
    
    * new 1
    
    * new 2
    
    * new change 3
    
    ---------
    
    Co-authored-by: Arpad Miklos <AMiklos@myagworks.com>
    
    * Add new project + project reference
    
    * squash test
    
    * squash 2
    
    * s3
    
    ---------
    
    Co-authored-by: Arpad Miklos <AMiklos@myagworks.com>
    
    * change to 040
    
    ---------
    
    Co-authored-by: Arpad Miklos <AMiklos@myagworks.com>
    
    ---------
    
    Co-authored-by: Arpad Miklos <AMiklos@myagworks.com>

---
+++ ConsoleApp12/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Class1
+    {
+        // Change 1
+        // Change 2
+    }
+}
--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -1,8 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-
+// squash 3
 
 
 //change 1 in my FB
 //change 2 in my FB
+
+// release 16.030
+
+
+// Change to 040
+
+// NEW to 040
