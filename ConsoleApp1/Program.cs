79132e46c616e282db03b6bb77c79436c80521d5
miklosa <79603166+miklosa@users.noreply.github.com>
2023-12-08 11:12:38 -0600

    Release/16.030 (#10)
    * Change 1
    
    * Change 2
    
    * Change 3
    
    * change 4
    
    * Miklosa/release 16.030/123455 (#8)
    
    * new 1
    
    * new 2
    
    * new change 3
    
    ---------
    
    Co-authored-by: Arpad Miklos <AMiklos@myagworks.com>
    
    * Add new project + project reference
    
    * squash test
    
    * squash 2
    
    * s3
    
    ---------
    
    Co-authored-by: Arpad Miklos <AMiklos@myagworks.com>

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
-// squash 3
-
-
-//change 1 in my FB
-//change 2 in my FB
-
-// release 16.030
-
-
-// Change 3
-
-// NEW 3
