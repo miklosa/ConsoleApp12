2a89ca9f01a60cd9c6ca628cf33e4de87b573bda
Arpad Miklos <AMiklos@myagworks.com>
2023-12-11 07:54:29 -0600

    2nd file

---
+++ ConsoleApp1/Class4.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class4
+    {
+        // simple merge 2nd file
+    }
+}
